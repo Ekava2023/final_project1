bad1afb4981b2d615ed364b1ced593b160d47361
Ekaterina Vakhromeeva <ekava20@yandex.ru>
2024-02-17 20:13:52 +0300

    program in c# is tested

--- Program.cs
+++ Program.cs
@@ -4,3 +4,32 @@
 //   ввести с клавиатуры, либо задать на старте 
 //   выполнения алгоритма. 
 
+string[] matrix1 = { "ya", ":-)", "text", "yes", "no", "london", "wow" };
+int symbol_quantity = 3;
+
+string[] buildArrayOfShortWords(string[] matrix, int len)
+{
+    int count_short_words = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (matrix1[i].Length <= len)
+        {
+            count_short_words++;
+        }
+    }
+    Console.WriteLine($"Количество слов с длиной символов не более {symbol_quantity} равно {count_short_words}");
+    
+    string[] matrix2 = new string[count_short_words];
+    int j = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (matrix1[i].Length <= len)
+        {
+            matrix2[j] += matrix[i];
+            j++;
+        }
+    }
+    return matrix2;
+}
+
+Console.WriteLine($"Массив выбранных слов: [{string.Join(";", buildArrayOfShortWords(matrix1, symbol_quantity))}]");
